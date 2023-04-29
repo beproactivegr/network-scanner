81f96c543e61a0ca2ecb97529eafd6689b03ee1e
beproactivegr <social@beproactive.gr>
2023-04-29 21:22:53 +0300

    init

---
+++ network-scanner.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+"""
+	Network Scanner
+    Copyright (c) 2023 @beproactivegr
+	https://github.com/beproactivegr/network-scanner
+
+    A free and open source utility for network discovery.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    For more see the file 'LICENSE' for copying permission.
+"""
+
+__author__ = "beproactivegr"
+__copyright__ = "Copyright (c) 2023 @beproactivegr"
+__credits__ = ["beproactivegr"]
+__license__ = "GPLv3"
+__version__ = "1.0"
+__maintainer__ = "beproactivegr"
+
+################################
+
+
+
+################################
+
+if __name__ == '__main__':
+
+	try:
+
+	except KeyboardInterrupt:
+        sys.exit(0)
